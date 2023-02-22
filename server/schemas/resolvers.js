054e5e127c47902efd9ada6949c7c3550c21a2c3
Sean Collins <109634748+iseanc@users.noreply.github.com>
2023-02-21 21:22:46 -0700

    resolvers done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,54 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User} = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
  Query: {
-  me: async () => {
+  users: async () => {
     return await User.find({});
-  }
+  },
+  me: async (parent, { email } ) => {
+    return await User.findOne({ email: email });
+  },
+ },
+
+ Mutation: {
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user with this email found!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+  },
+  addUser: async (parent, { username, email, password }) => {
+    const user = await User.create({ username, email, password });
+    const token = signToken(user);
+
+    return { token, user };
+  },
+  saveBook: async (parent, { userId, book }) => {
+    return User.findOneAndUpdate(
+      { _id: userId},
+      { $addToSet: { savedBooks: book} },
+      { new: true, runValidators: true }
+    );
+  },
+  removeBook: async (parent, { userId, bookId}) => {
+    return User.findOneAndUpdate(
+      { _id: userId},
+      { $pull: {savedBooks: {bookId: bookId}}},
+      { returnDocument: true }
+    );
+  },
  }
 }
 
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 // TODO: Determine if /routes can be removed
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // app.use(routes);
 
